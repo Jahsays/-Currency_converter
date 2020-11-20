4638da4c230d4c6996c557354f1c391ff1745e87
Chiaka Williams <Chiaka394@gmail.com>
2020-11-20 17:18:14 +0100

    reworked the whole thing

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const base_currencyE1 = document.getElementById('base_currency');
-const base_amountE1 = document.getElementById('base_amount');
-const quote_currencyE1 = document.getElementById('quote_currency');
-const quote_amountE1 = document.getElementById('quote_amount');
-const rateE1 = document.getElementById('rate');
+const from_currencyEl = document.getElementById('from_currency');
+const from_ammountEl = document.getElementById('from_ammount');
+const to_currencyEl = document.getElementById('to_currency');
+const to_ammountEl = document.getElementById('to_ammount');
+const rateEl = document.getElementById('rate');
 const exchange = document.getElementById('exchange');
 
-base_currencyE1.addEventListener('change', calculate);
-base_amountE1.addEventListener('change', calculate);
-quote_currencyE1.addEventListener('change', calculate);
-quote_amountv.addEventListener('change', calculate);
+from_currencyEl.addEventListener('change', calculate);
+from_ammountEl.addEventListener('input', calculate);
+to_currencyEl.addEventListener('change', calculate);
+to_ammountEl.addEventListener('input', calculate);
 
 exchange.addEventListener('click', () => {
-const temp = base_currencyE1.value;
-base_currencyE1.value = quote_currencyE1.value;
-quote_currencyE1.value = temp;
-calculate();
+	const temp = from_currencyEl.value;
+	from_currencyEl.value = to_currencyEl.value;
+	to_currencyEl.value = temp;
+	calculate();
 });
 
 function calculate() {
-    const base_currency = base_currencyE1.value;
-    const quote_currency = quote_currencyE1.value;
-
-    fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`)
-    .then(res => res.json())
-    .then(res => {
-     const rate = res.rates[quote_currency];
-     rateE1.innerText = `1 ${base_currency} = ${rate} ${quote_currency}`
-     quote_amountE1.value = (base_amountE1.value * rate).toFixed(2);
-    })
+	const from_currency = from_currencyEl.value;
+	const to_currency = to_currencyEl.value;
+	
+	fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`)
+		.then(res => res.json())
+		.then(res => {
+		const rate = res.rates[to_currency];
+		rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`
+		to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
+	})
 }
 
 calculate();
