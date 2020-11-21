2b157f1136c71f2f74c56cfcd089292a7648e241
Chiaka Williams <Chiaka394@gmail.com>
2020-11-21 18:42:47 +0100

    tweaked the html and js file

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const from_currencyEl = document.getElementById('from_currency');
-const from_ammountEl = document.getElementById('from_ammount');
-const to_currencyEl = document.getElementById('to_currency');
-const to_ammountEl = document.getElementById('to_ammount');
+const base_currencyEl = document.getElementById('base_currency');
+const base_amountEl = document.getElementById('base_amount');
+const quote_currencyEl = document.getElementById('quote_currency');
+const quote_amountEl = document.getElementById('quote_amount');
 const rateEl = document.getElementById('rate');
 const exchange = document.getElementById('exchange');
 
-from_currencyEl.addEventListener('change', calculate);
-from_ammountEl.addEventListener('input', calculate);
-to_currencyEl.addEventListener('change', calculate);
-to_ammountEl.addEventListener('input', calculate);
+base_currencyEl.addEventListener('change', calculate);
+base_amountEl.addEventListener('input', calculate);
+quote_currencyEl.addEventListener('change', calculate);
+quote_amountEl.addEventListener('input', calculate);
 
 exchange.addEventListener('click', () => {
-	const temp = from_currencyEl.value;
-	from_currencyEl.value = to_currencyEl.value;
-	to_currencyEl.value = temp;
+	const temp = base_currencyEl.value;
+	base_currencyEl.value = quote_currencyEl.value;
+	quote_currencyEl.value = temp;
 	calculate();
 });
 
 function calculate() {
-	const from_currency = from_currencyEl.value;
-	const to_currency = to_currencyEl.value;
+	const base_currency = base_currencyEl.value;
+	const quote_currency = quote_currencyEl.value;
 	
-	fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`)
+	fetch(`https://api.exchangerate-api.com/v4/latest/${base_currency}`)
 		.then(res => res.json())
 		.then(res => {
-		const rate = res.rates[to_currency];
-		rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`
-		to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
+		const rate = res.rates[quote_currency];
+		rateEl.innerText = `1 ${base_currency} = ${rate} ${quote_currency}`
+		quote_amountEl.value = (base_amountEl.value * rate).toFixed(2);
 	})
 }
 
